a9550c6b99176859fbfe419916fe8fa144ca66da
Ryan Smith <rksmith369@gmail.com>
2019-05-09 22:20:10 -0700

    Created react app, added todo list and functionality.

---
+++ src/components/Todos.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
+// import logo from './logo.svg';
+
+
+class Todos extends Component {
+
+  render(){
+    //   console.log(this.props.todos)
+    return this.props.todos.map((todo) => (        // Just like a for each function:, we are iterating through the todo with map.
+        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo}/>
+    ))     
+  }
+} 
+
+// Prop Types:
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
+
